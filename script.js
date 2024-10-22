ca111fa9fcce1f441816fb1c2e2774be7c137ba1
Piyush Nishad <businessmarksea@gmail.com>
2024-10-22 12:29:52 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+let startTime, updatedTime, difference, tInterval, running = false;
+
+const timeDisplay = document.getElementById('time');
+const startStopBtn = document.getElementById('startStopBtn');
+const resetBtn = document.getElementById('resetBtn');
+
+function startStop() {
+    if (!running) {
+        startTime = new Date().getTime();
+        tInterval = setInterval(updateTime, 1000);
+        startStopBtn.innerHTML = 'Stop';
+        running = true;
+    } else {
+        clearInterval(tInterval);
+        startStopBtn.innerHTML = 'Start';
+        running = false;
+    }
+}
+
+function updateTime() {
+    updatedTime = new Date().getTime();
+    difference = updatedTime - startTime;
+    
+    let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    timeDisplay.innerHTML = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
+
+function reset() {
+    clearInterval(tInterval);
+    running = false;
+    timeDisplay.innerHTML = "00:00:00";
+    startStopBtn.innerHTML = 'Start';
+}
+
+startStopBtn.addEventListener('click', startStop);
+resetBtn.addEventListener('click', reset);
